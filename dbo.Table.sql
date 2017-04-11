c73e4b059e3fb05170d4898e742d755c2acb49a2
GregT <Greg@T.com>
2017-04-11 11:49:23 -0400

    Tables DB

---
+++ dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Products]
+(
+	[Id] NVARCHAR(50) NOT NULL PRIMARY KEY, 
+    [PruductName] NVARCHAR(50) NOT NULL, 
+    [Catagory] NVARCHAR(50) NOT NULL, 
+    [SupplierName] NVARCHAR(50) NOT NULL, 
+    [Price] SMALLMONEY NOT NULL, 
+    [Quantity] INT NOT NULL, 
+    [Location] NVARCHAR(50) NOT NULL
+)
