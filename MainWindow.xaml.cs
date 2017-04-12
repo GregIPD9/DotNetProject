b7598d182e11013f0424f241d224669affe7ec4d
Andrei Sinkevich <storekeeper1979@gmail.com>
2017-04-11 20:11:42 -0400

    *Trying to connect to DB using LogIn window

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
 
 namespace InventoryApp
 {
@@ -27,8 +28,37 @@ namespace InventoryApp
 
         private void buttonLogIn_Click(object sender, RoutedEventArgs e)
         {
-            ManagerView managerView = new ManagerView();
-            managerView.Show();
+            SqlConnection cn = new SqlConnection("Server=tcp:andrei-greg.database.windows.net,1433;Initial Catalog=InventoryDB;Persist Security Info=False;User ID={DBadmin};Password={JohnIsGreat2000};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            cn.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.users WHERE user = '"+ tbUser.Text + "' AND password = '"+tbPassword.Text + "'", cn);
+            SqlDataReader dr;
+            dr = cmd.ExecuteReader();
+            int count = 0;
+            while (dr.Read())
+            {
+                count += 1;
+            }
+            if (count == 1)
+            {
+                MessageBox.Show("ok");
+                MainSIMS.AdminView adminView = new MainSIMS.AdminView();
+                adminView.Show();
+            }
+            else if (count > 0)
+            {
+                MessageBox.Show("Duplicate username and password");
+            }
+            else
+            {
+                MessageBox.Show("username or password not correct");
+            }
+            tbUser.Clear();
+            tbPassword.Clear();
+
+
+
+            // ManagerView managerView = new ManagerView();
+            //  managerView.Show();
         }       
     }
 }
