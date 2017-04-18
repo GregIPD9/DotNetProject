d8184fae2581c9d958305e2d79714fee86f9d4d7
GregT <Greg@T.com>
2017-04-18 11:02:36 -0400

    Load Products on EmployeeView*ManagerView

--- EmployeeView.xaml.cs
+++ EmployeeView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using MainSIMS;
+using System.Data.SqlClient;
 
 namespace MainSIMS
 {
@@ -20,9 +21,62 @@ namespace MainSIMS
     /// </summary>
     public partial class EmployeeView : Window
     {
+        Database db;
         public EmployeeView()
         {
-            InitializeComponent();
+            try
+            {
+                db = new Database();
+                InitializeComponent();
+
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.StackTrace);
+                MessageBox.Show("Error opening database connection: " + e.Message);
+                Environment.Exit(1);
+            }
+        }
+
+        private void btloadProducts_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "andrei-greg.database.windows.net";
+                builder.UserID = "DBadmin";
+                builder.Password = "JohnIsGreat2000";
+                builder.InitialCatalog = "InventoryDB";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nQuery data example:");
+                    Console.WriteLine("=========================================\n");
+
+                    connection.Open();
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("SELECT ProductName, Location FROM [dbo].[Products]");
+                    String sql = sb.ToString();
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+
+
+                                lvProductList.ItemsSource = db.GetAllProducts();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
 
        
